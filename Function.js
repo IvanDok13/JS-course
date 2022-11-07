2d052a5f1837ef9f6ef2f29add541e054c1f6476
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-11-08 00:52:09 +0300

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,9 @@
+//Display the absolute value of the number contained in the soldiersCount constant
+
+
+const soldiersCount = -2309;
+
+const result = Math.abs(soldiersCount);
+console.log(result);
+
+//=>2309
