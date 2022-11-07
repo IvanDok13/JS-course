4cf344f49c5bcf1236a4f727c822ea36e8ad6f8e
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-11-08 00:08:47 +0300

    Add files via upload

---
+++ Immutability and primitive types.js
@@ -0,0 +1,16 @@
+/*You are given three variables with the last names of different people. 
+Compose and display a word from the characters in this order:
+
+Third character from the first line
+Second character from second line
+Fourth character from the third line
+Fifth character from the second line
+The third character from the second line*/
+
+const one = 'Naharis';
+const two = 'Mormont';
+const three = 'Sand';
+
+console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`)
+
+//"hodor"
