4277770c08c09808714f70fed688d6f0a0bd0460
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-11-08 20:49:57 +0300

    Update Function.js

--- Function.js
+++ Function.js
@@ -1,9 +1,13 @@
 //Display the absolute value of the number contained in the soldiersCount constant
-
-
 const soldiersCount = -2309;
-
 const result = Math.abs(soldiersCount);
-console.log(result);
 
+console.log(result);
 //=>2309
+
+
+//Math has a ceil() function. Check out its documentation and write code using const nuber
+const number = 923.2238;
+
+console.log(Math.ceil(number));
+//924
