babddd1969cf900e304374d61e4ca6bf113e67fd
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-11-12 15:18:55 +0300

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,16 @@
+//Implement a function called printMotto() that will print the phrase Winter is coming to the screen.
+const printMotto = () => {
+  const motto = 'Winter is coming';
+  console.log(motto);
+};
+
+printMotto();
+//Winter is coming
+
+//OR
+const printMotto1 = () => {
+    console.log('Winter is coming');
+};
+
+printMotto1();
+//Winter is coming
