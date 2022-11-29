25dcf0c312b80c1273122857eb382a026b85bf16
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-11-29 22:47:05 +0300

    Update and rename Сoncatenation.js to Сoncatenation-Interpolation.js

---
+++ Сoncatenation-Interpolation.js
@@ -0,0 +1,12 @@
+                                                     //Concatenation
+const king = 'King Balon the 6th';
+const kingCount = 6;
+const castlesCount = 17;
+
+console.log(king + ' has ' + kingCount * castlesCount + ' rooms.');
+//"King Balon the 6th has 102 rooms."
+
+                                                     //Interpolation
+const stark = 'Arya';
+console.log(`Do you want to eat, ${stark}?`)
+//"Do you want to eat, Arya?"
--- Сoncatenation.js
+++
@@ -1,7 +0,0 @@
-const king = 'King Balon the 6th';
-const kingCount = 6;
-const castlesCount = 17;
-
-console.log(king + ' has ' + kingCount * castlesCount + ' rooms.');
-
-//"King Balon the 6th has 102 rooms."
