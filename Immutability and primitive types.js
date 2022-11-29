207549397201f36d1bab2bd7d470ea7c56bafdb8
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-11-29 22:40:09 +0300

    Update Immutability and primitive types.js

--- Immutability and primitive types.js
+++ Immutability and primitive types.js
@@ -14,3 +14,23 @@ const three = 'Sand';
 console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`)
 
 //"hodor"
+
+                                                                    //Charter extraction
+//Display my name
+
+const name1= 'Jon';
+const name2= 'Iuda';
+const name3= 'Vinsent.vangog';
+
+console.log(name2[0] + name3[8] + name3[9] + name1[2]);
+
+//"Ivan"
+
+
+//Display the last character of the string contained in the word constant.
+
+const word = 'Na\nharis';
+
+console.log(word[7]);
+
+//"s"
