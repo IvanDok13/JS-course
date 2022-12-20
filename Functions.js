7dd6faefa0e160ef7d60b741f71160b6b29713a6
Ivan Klochkov <113414074+IvanDok13@users.noreply.github.com>
2022-12-20 23:03:51 +0300

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,23 +1,6 @@
                                                               //Basic Function
 
-//Implement a function called printMotto() that will print the phrase Winter is coming to the screen.
-const printMotto = () => {
-  const motto = 'Winter is coming';
-  console.log(motto);
-};
-
-printMotto();
-//Winter is coming
-
-//OR
-const printMotto1 = () => {
-    console.log('Winter is coming');
-};
-
-printMotto1();
-//Winter is coming
-
-                                                     //Сoncatenation `${...}${...}, end of the text.`  inside the Function
+                                           //Сoncatenation `${...}${...}, end of the text.`  inside the Function
 
 //Implement a function that prints hurray! 3 times
 const sayHurrayThreeTimes = () => {
